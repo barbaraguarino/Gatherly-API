3aa2314fb520b4dfef7676fc3b0e21edce62e552
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-07-24 20:27:12 -0300

    Create UserRepositoryPort.java

---
+++ src/main/java/com/guarino/gatherlyapi/application/port/out/UserRepositoryPort.java
@@ -0,0 +1,17 @@
+package com.guarino.gatherlyapi.application.port.out;
+
+import com.guarino.gatherlyapi.domain.model.user.User;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepositoryPort {
+
+    User save(User user);
+
+    Optional<User> findById(UUID id);
+
+    Optional<User> findByEmail(String email);
+
+    boolean existsByEmail(String email);
+}
