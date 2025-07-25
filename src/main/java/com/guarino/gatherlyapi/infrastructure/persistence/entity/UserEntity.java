cf913df1e78fffa24a17b1c702042edfe59eb8ec
Barbara Guarino <40583024+barbaraguarino@users.noreply.github.com>
2025-07-24 21:50:51 -0300

    Create UserEntity.java

---
+++ src/main/java/com/guarino/gatherlyapi/infrastructure/persistence/entity/UserEntity.java
@@ -0,0 +1,48 @@
+package com.guarino.gatherlyapi.infrastructure.persistence.entity;
+
+import com.guarino.gatherlyapi.domain.model.user.UserRole;
+import com.guarino.gatherlyapi.domain.model.user.UserStatus;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(name = "password_hash", nullable = false)
+    private String passwordHash;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserRole role;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private UserStatus status;
+
+    @Column(name = "created_at", nullable = false, updatable = false)
+    private ZonedDateTime createdAt;
+
+    @Column(name = "updated_at", nullable = false)
+    private ZonedDateTime updatedAt;
+}
